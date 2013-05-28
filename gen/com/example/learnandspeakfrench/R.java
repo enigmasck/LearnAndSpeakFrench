5486ff4e1defcbbfb235f974b965d4e581c0f308
enigmasck <timothyasanders@gmail.com>
2013-05-28 16:16:28 -0400

    Working on main menu. Added onClick Listeners, and starting new intents

--- gen/com/example/learnandspeakfrench/R.java
+++ gen/com/example/learnandspeakfrench/R.java
@@ -11,19 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ib_grammar=0x7f020000;
-        public static final int ib_verbs=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int lsf_grammar=0x7f020003;
-        public static final int lsf_phrases=0x7f020004;
-        public static final int lsf_verbs=0x7f020005;
-        public static final int lsf_vocab=0x7f020006;
+        public static final int ib_convo=0x7f020000;
+        public static final int ib_grammar=0x7f020001;
+        public static final int ib_tests=0x7f020002;
+        public static final int ib_verbs=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int lsf_grammar=0x7f020005;
+        public static final int lsf_phrases=0x7f020006;
+        public static final int lsf_verbs=0x7f020007;
+        public static final int lsf_vocab=0x7f020008;
     }
     public static final class id {
+        public static final int ib_convo=0x7f070004;
         public static final int ib_grammar=0x7f070000;
+        public static final int ib_phrases=0x7f070003;
+        public static final int ib_tests=0x7f070005;
         public static final int ib_verbs=0x7f070002;
         public static final int ib_vocab=0x7f070001;
-        public static final int menu_settings=0x7f070003;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +39,10 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int ib_cd_convo=0x7f040007;
         public static final int ib_cd_grammar=0x7f040004;
         public static final int ib_cd_phrases=0x7f040006;
+        public static final int ib_cd_tests=0x7f040008;
         public static final int ib_cd_verbs=0x7f040005;
         public static final int ib_cd_vocab=0x7f040003;
         public static final int menu_settings=0x7f040002;
--- src/com/example/learnandspeakfrench/MainActivity.java
+++ src/com/example/learnandspeakfrench/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.learnandspeakfrench;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	//instantiate main menu buttons
+	Button ibVocab = (Button)findViewById(R.id.ib_vocab);
+	Button ibGrammar = (Button)findViewById(R.id.ib_grammar);
+	Button ibVerbs = (Button)findViewById(R.id.ib_verbs);
+	Button ibPhrases = (Button)findViewById(R.id.ib_phrases);
+	Button ibConvo = (Button)findViewById(R.id.ib_convo);
+	Button ibTests = (Button)findViewById(R.id.ib_tests);
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //add button listeners
+    	ibVocab.setOnClickListener(this);
+    	ibGrammar.setOnClickListener(this);
+    	ibVerbs.setOnClickListener(this);
+    	ibPhrases.setOnClickListener(this);
+    	ibConvo.setOnClickListener(this);
+    	ibTests.setOnClickListener(this);
     }
 
     @Override
@@ -18,5 +39,13 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void startVocabMenu(View v){
+    	Intent vocabMenuIntent = new Intent(this, VocabMenuActivity.class);
+    	startActivity(intent);
+    }
+
+	
+	
     
 }
